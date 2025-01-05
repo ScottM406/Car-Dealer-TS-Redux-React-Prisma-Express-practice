ccd3f9ae33db13adb8b2795dbbfee037126950b5
Scott <Scottyjmt13@gmail.com>
2025-01-04 19:51:30 -0700

    Add bcrypt, jwt dependancies. Add auth express routes (auth.js) and login/reg methods.

--- cardealer-backend/prisma/index.js
+++ cardealer-backend/prisma/index.js
@@ -1,5 +1,27 @@
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  model: {
+    user: {
+      async register(email, password) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({
+          data: { email, password: hashedPassword},
+        });
+        return user;
+      },
+
+      async login(email, password) {
+        const user = await prisma.user.findUniqueOrThrow({
+          where: { email },
+        });
+        const validCredentials = await bcrypt.compare(password, user.password);
+        if (!validCredentials) throw Error("Invalid Credentials");
+        return user;
+      }
+    }
+  }
+});
 
 module.exports = prisma;
