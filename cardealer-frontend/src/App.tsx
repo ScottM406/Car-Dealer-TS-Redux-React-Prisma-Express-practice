a6b0dd3fca40e89a408a10ae4c6034eb3ce17c50
Scott <Scottyjmt13@gmail.com>
2025-01-07 19:05:15 -0700

    Add Routes and register route to App.tsx

--- cardealer-frontend/src/App.tsx
+++ cardealer-frontend/src/App.tsx
@@ -1,8 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+
 const App = () => {
 
   return (
     <>
       <h1>Welome to Car Dealer!</h1>
+      <Routes>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
     </>
   )
 }
