dc8f83033239ddadd1f72cca48c6e1d12a8067db
Scott <Scottyjmt13@gmail.com>
2025-01-07 20:29:04 -0700

    Add Login functionality to Login.tsx.

--- cardealer-frontend/src/components/Login.tsx
+++ cardealer-frontend/src/components/Login.tsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("")
   const [token, setToken] = useState<string>("");
 
-  const logInUser = async () => {
+  const logInUser = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-  }
+    const response = await fetch("http://localhost:3000/login", ({
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+    }));
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+    const token = responseJSON.token;
+    setToken(token);
+  };
 
   return (
     <>
