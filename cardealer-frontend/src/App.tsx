a4876cd99049df20c20b14330a3a08312dc615db
Scott <Scottyjmt13@gmail.com>
2025-01-08 21:34:19 -0700

    Add 'Add Inventory' route to App.tsx.

--- cardealer-frontend/src/App.tsx
+++ cardealer-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
+import AddInventroy from "./components/AddInventory";
 
 const App = () => {
 
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/addinventory" element={<AddInventroy />} />
       </Routes>
     </>
   )
