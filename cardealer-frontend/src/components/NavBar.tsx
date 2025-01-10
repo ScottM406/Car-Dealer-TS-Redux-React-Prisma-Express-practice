61cba0ff3ef9c967fbf2c61c32e0d2885b5abeb7
Scott <Scottyjmt13@gmail.com>
2025-01-10 14:44:18 -0700

    Add 'addinventory' link to super user Navbar.

--- cardealer-frontend/src/components/NavBar.tsx
+++ cardealer-frontend/src/components/NavBar.tsx
@@ -12,6 +12,9 @@ const NavBar: React.FC<Props> = ({ token, isSuperUser }) => {
       <Link to="/">Home</Link>
       <Link to="/register">Register</Link>
       <Link to="/login">Log In</Link>
+      {token && isSuperUser && (
+        <Link to="/addinventory">Add Inventory</Link>
+      )}
     </>
   )
 }
