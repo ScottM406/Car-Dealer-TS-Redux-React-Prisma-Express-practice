aa5ccadf9b96fba8c00ff78ec1fa0d678d207f21
Scott <Scottyjmt13@gmail.com>
2025-01-10 15:18:37 -0700

    Add ProtectedRoute.tsx. Protect AddInventory route.

--- cardealer-frontend/src/App.tsx
+++ cardealer-frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
 import AddInventory from "./components/AddInventory";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   const [token, setToken] = useState<string>("");
@@ -18,7 +19,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login token={token} setToken={setToken} setIsSuperUser={setIsSuperUser}/>} />
-        <Route path="/addinventory" element={<AddInventory />} />
+        <Route path="/addinventory" element={<ProtectedRoute component={AddInventory} isSuperUser={isSuperUser}/>} />
       </Routes>
     </>
   )
