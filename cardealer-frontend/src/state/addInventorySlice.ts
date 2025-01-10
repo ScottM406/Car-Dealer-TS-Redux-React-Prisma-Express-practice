e5b78de0bcedb2386eafb00019110c73592a8cdc
Scott <Scottyjmt13@gmail.com>
2025-01-09 18:16:42 -0700

    Create addInverntoySlice.ts.

---
+++ cardealer-frontend/src/state/addInventorySlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AddInverntoryState {
+  headline: string;
+  description: string;
+  image: string;
+  year: number;
+  miles: number;
+  drivetrain: string;
+  engine: string;
+  color: string;
+  MPG_city: number;
+  MPG_highway: number;
+  modelID: number;
+  features: string;
+  price: number;
+}
+
+const initialState: AddInverntoryState = {
+  headline: "",
+  description: "",
+  image: "",
+  year: 0,
+  miles: 0,
+  drivetrain: "",
+  engine: "",
+  color: "",
+  MPG_city: 0,
+  MPG_highway: 0,
+  modelID: 0,
+  features: "",
+  price: 0
+}
+
+const addInventorySlice = createSlice({
+  name: "addInventory",
+  initialState,
+  reducers: {
+    setInputValue: (state, action: PayloadAction<{ field: keyof AddInverntoryState; value: any}>) => {
+      (state[action.payload.field] as typeof state[keyof AddInverntoryState]) = action.payload.value;
+    }
+  }
+})
+
+export const { setInputValue } = addInventorySlice.actions
+export default addInventorySlice.reducer
