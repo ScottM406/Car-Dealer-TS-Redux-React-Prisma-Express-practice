6db5c1cebab404063bce683ee4e5dace4b35f246
Scott <Scottyjmt13@gmail.com>
2025-01-10 14:09:23 -0700

    Pass login token props from App.tsx to Navbar.tsx and Login.tsx.

--- cardealer-frontend/src/App.tsx
+++ cardealer-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import NavBar from "./components/NavBar";
 import Register from "./components/Register";
 import Login from "./components/Login";
@@ -6,14 +7,15 @@ import LandingPage from "./components/LandingPage";
 import AddInventory from "./components/AddInventory";
 
 const App = () => {
+  const [token, setToken] = useState<string>("");
 
   return (
     <>
-    <NavBar />
+    <NavBar token={token}/>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login token={token} setToken={setToken}/>} />
         <Route path="/addinventory" element={<AddInventory />} />
       </Routes>
     </>
