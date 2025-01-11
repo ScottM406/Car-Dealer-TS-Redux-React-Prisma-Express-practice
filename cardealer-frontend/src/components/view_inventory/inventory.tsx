0cfd17010bca39e1edbddce82adcf2e6615aca1f
Scott <Scottyjmt13@gmail.com>
2025-01-10 17:04:39 -0700

    Add vehiclesSlice.ts. Add inventory reducers.

--- cardealer-frontend/src/components/view_inventory/inventory.tsx
+++ cardealer-frontend/src/components/view_inventory/inventory.tsx
@@ -1,9 +1,24 @@
+import { getVehicles } from "../../state/vehiclesSlice";
+import { AppDispatch, RootState } from "../../state/store";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
 const Inventory: React.FC = () => {
-  
+
+  const vehicles = useSelector((state: RootState) => state.vehicles)
+  const dispatch = useDispatch<AppDispatch>();
+
+
+  useEffect(() => {
+    dispatch(getVehicles());
+  }, [])
+
+  console.log(vehicles);
+
   return (
     <>
     <h2>Vehicles for Sale</h2>
-    <p>Click on vehicle for more details.</p>
+    <p>Clveick on vehicle for more details.</p>
     </>
   )
 }
