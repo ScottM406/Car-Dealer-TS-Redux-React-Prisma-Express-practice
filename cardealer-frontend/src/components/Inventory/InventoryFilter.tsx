e3511272e59a6696ae2c72c3908b570360bef68f
Scott <Scottyjmt13@gmail.com>
2025-01-15 17:03:27 -0700

    Add filter form, filter by model functionality to InventoryFilter.tsx

--- cardealer-frontend/src/components/Inventory/InventoryFilter.tsx
+++ cardealer-frontend/src/components/Inventory/InventoryFilter.tsx
@@ -1,8 +1,45 @@
+import { AppDispatch, RootState } from "../../state/store";
+import { useDispatch, useSelector } from "react-redux"; 
+import { useState } from "react";
+import { getMakes } from "../../state/makesSlice";
+
 const InventoryFilter = () => {
+  const [selectedMake, setSelectedMake] = useState("")
+
+  const makes = useSelector((state: RootState) => state.makes)
+  const dispatch = useDispatch<AppDispatch>();
+  const fileteredModels = makes.find((make) => make.name === selectedMake)?.models || [];
+
+  console.log(selectedMake);
+  console.log(fileteredModels)
 
   return (
-    <h3>TEST (INVENOTRY FILTER)</h3>
+    <form>
+      <h4>Filter Vehicles</h4>
+      <label>Make: </label>
+      <select onChange={(e) => setSelectedMake(e.target.value)} onClick={() => dispatch(getMakes())}>
+        <option>Select Make</option>
+        {makes.map((make) => (
+          <option key={make.name}>{make.name}</option>
+        ))}
+      </select>
+      <label>Model</label>
+      <select>
+        <option>test</option>
+      </select>
+    </form>
   )
 }
 
 export default InventoryFilter
+
+//OLD CODE
+
+  // const createMakesSelector = () => {
+  //   useEffect(() => {
+  //     dispatch(getMakes());
+  //   }, [])
+
+  // }
+  
+  // console.log(createMakesSelector())
