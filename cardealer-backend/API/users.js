2d22d4a26802f37b842f389e58938c1413e1f78c
Scott <Scottyjmt13@gmail.com>
2025-01-17 18:43:36 -0700

    Fixed typo in get user by ID route - /id to /:id.

--- cardealer-backend/API/users.js
+++ cardealer-backend/API/users.js
@@ -13,7 +13,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/id", authenticate, async (req, res, next) => {
+router.get("/:id", authenticate, async (req, res, next) => {
   const { id } = req.params;
   try {
     if (req.user.id !== Number(id)) {
