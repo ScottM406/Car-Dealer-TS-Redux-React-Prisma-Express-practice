980fc2e36fb212eca6f95ca180bea8ee27ec6ec6
Scott <Scottyjmt13@gmail.com>
2025-01-20 21:01:52 -0700

    Render watchlist to Account.tsx.

--- cardealer-frontend/src/components/Account.tsx
+++ cardealer-frontend/src/components/Account.tsx
@@ -7,12 +7,12 @@ interface AccountProps {
 
 interface UserInfo {
   email: string
-  watchlist: object[]
-  
+  watchlist:  { id: number, userID: number }
 }
 
 const Account: React.FC<AccountProps> = ({ userID, token }) => {
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const [watchlist, setWatchlist] = useState<Array<object> | null>(null);
 
   useEffect(() => {
     const getUserInfo = async () => {
@@ -22,19 +22,33 @@ const Account: React.FC<AccountProps> = ({ userID, token }) => {
         }
       });
       const responseJSON = await response.json();
-      console.log(responseJSON)
       setUserInfo(responseJSON);
     }
     getUserInfo();
   }, [])
 
+  useEffect(() => {
+    const getWatchlist = async () => {
+      const response = await fetch(`http://localhost:3000/watchlists/${userInfo?.watchlist.id}`)
+      const responseJSON = await response.json();
+      setWatchlist(responseJSON.cars)
+    }
+    getWatchlist();
+  }, [userInfo])
+
+  console.log(watchlist)
+
   return (
     <>
-      <h1>TEST</h1>
       <h2>{userInfo?.email}</h2>
-      {/* {userInfo?.watchlist.map((vehicle) => (
-
-      )} */}
+      <h2>Watchlist</h2>
+      {watchlist?.map((vehicle: any) => (
+        <div key={vehicle.CarsOnLot.id}>
+          <img src={vehicle.CarsOnLot.image} style={{ width:"400px", height: "250px" }} />
+          <h5>{vehicle.CarsOnLot.headline}</h5>
+          <h4>${vehicle.CarsOnLot.price}</h4>
+        </div>
+      ))}
     </>
   )
 }
