73b54760f5ea3d1ebdf9feed0ea38a6691c288f8
Scott <Scottyjmt13@gmail.com>
2025-01-24 20:59:43 -0700

    Add add makes and add models pages. Debug admin <Routes> - now working.

--- cardealer-frontend/src/components/admin-toolkit/AdminToolkit.tsx
+++ cardealer-frontend/src/components/admin-toolkit/AdminToolkit.tsx
@@ -1,20 +1,28 @@
-import { Routes, Route, Link } from "react-router-dom"
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import AddInventory from "./AddInventory";
+import AddMake from "./AddMake";
+import AddModel from "./AddModel";
 
 const AdminToolkit = () => {
+  const location = useLocation();
+  const basePath = location.pathname.split('/').slice(0, 2).join('/');
 
   return (
     <>
+      <div id="admin-toolkit-block">
+        <h1>Admin Toolkit</h1>
+        <Link to={`${basePath}/addinventory`}>Add Inventory</Link>
+        <Link to={`${basePath}/addmake`}>Add Make</Link>
+        <Link to={`${basePath}/addmodel`}>Add Model</Link>
+      </div>
+
       <Routes>
         <Route path="addinventory" element={<AddInventory />} />
+        <Route path="addmake" element={<AddMake />} />
+        <Route path="addmodel" element={<AddModel />} />
       </Routes>
-
-    <div id="admin-toolkit-block">
-      <h1>Test</h1>
-      <Link to="addinventory">Add Inventory</Link>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default AdminToolkit;
