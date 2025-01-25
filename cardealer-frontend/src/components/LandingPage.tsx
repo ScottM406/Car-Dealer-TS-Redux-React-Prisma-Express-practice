848a6e221e0c3a4a0e75748cca6fb27332ec59ee
Scott <Scottyjmt13@gmail.com>
2025-01-24 19:20:33 -0700

    Add landing page content and render. Style landing page.

--- cardealer-frontend/src/components/LandingPage.tsx
+++ cardealer-frontend/src/components/LandingPage.tsx
@@ -1,7 +1,21 @@
 const LandingPage = () => {
 
   return (
-    <h1>Welome to Car Dealer!</h1>
+    <div id="landing-page-main-block">
+      <h1>Welome to Grand Avenue Auto Gallery!</h1>
+      <img src="src/assets/landing-page-photo.jpg" alt="The car lot at Grand Avenue Auto Gallery."/>
+      <p>
+        Here at Grand Anenue Auto Gallery, we consectetuer adipiscing elit. Blandit ad id vivamus, scelerisque magna integer adipiscing. 
+        Leo lacus consectetur nunc ultrices vestibulum potenti. Sagittis massa nulla vehicula quisque mattis arcu. Rutrum a nulla lobortis ante fermentum a. 
+        Dis dolor vel dapibus eros lobortis. Nullam pulvinar nulla enim; velit sodales massa? Litora faucibus pretium sed aliquam convallis. 
+        Imperdiet facilisi fusce luctus blandit venenatis erat. Nisi tincidunt augue gravida, ex massa felis.
+      </p>
+      <p>
+        Primis donec id eros maecenas egestas dui. Nullam mollis porttitor viverra cubilia pretium pretium magnis, metus dui. 
+        Ligula at rhoncus parturient blandit massa eget non. Facilisi lacus sagittis magna tellus tellus. Eleifend ipsum cursus leo a blandit. 
+        Dolor netus gravida magna per tortor proin est tristique. Nisi ornare pellentesque urna taciti lacinia tempor.
+      </p>
+    </div>
   )
 }
 
