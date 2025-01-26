119b94930354e6bbf35e6081fcc98237214646b5
Scott <Scottyjmt13@gmail.com>
2025-01-26 15:06:06 -0700

    Add add model funcitonality to AddModel.tsx.

--- cardealer-frontend/src/components/admin-toolkit/AddModel.tsx
+++ cardealer-frontend/src/components/admin-toolkit/AddModel.tsx
@@ -1,7 +1,69 @@
+import { FormEvent, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../../state/store";
+import { getMakes } from "../../state/makesSlice";
+
+
 const AddModel = () => {
+  const [newModelName, setNewModelName] = useState<string>("");
+  const [makeOfNewModel, setMakeofNewModel] = useState<string>("");
+  
+  const makes = useSelector((state: RootState) => state.makes)
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(getMakes());
+  }, [])
+
+  const resetForm = () => {
+    setNewModelName("");
+    setMakeofNewModel("");
+    alert("Model added successfully");
+  }
+
+  const addModelToDatabase = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/models", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          name: newModelName,
+          makeName: makeOfNewModel
+        })
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+
+    resetForm();
+
+    } catch(e: any) {
+      alert(e.message || "Something has gone wrong. Please try again later.")
+    }
+  };
 
   return (
-    <h1>TEST</h1>
+    <div className="add-inventory-block">
+      <h1>Add New Model to Database</h1>
+      <form onSubmit={addModelToDatabase}>
+        <label>Make:</label>
+        <select value={makeOfNewModel} onChange={(e) => setMakeofNewModel(e.target.value)}>
+          <option>Select Make</option>
+          {makes.map((make) => <option>{make.name}</option>)}
+        </select>
+        <label>Model Name:</label>
+        <input
+        required 
+        value={newModelName}
+        onChange={(e) => setNewModelName(e.target.value)}
+        />
+        <button type="submit">Add Model</button>
+      </form>
+    </div>
   )
 };
 
