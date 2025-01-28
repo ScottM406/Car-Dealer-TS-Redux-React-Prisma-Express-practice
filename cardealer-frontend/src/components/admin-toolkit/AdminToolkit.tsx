8b3ec65fa836ad8417033025d1c1a7d9b941e026
Scott <Scottyjmt13@gmail.com>
2025-01-27 21:30:15 -0700

    Add vehicledatabase route to admin toolkit and render link to vdb.

--- cardealer-frontend/src/components/admin-toolkit/AdminToolkit.tsx
+++ cardealer-frontend/src/components/admin-toolkit/AdminToolkit.tsx
@@ -3,6 +3,7 @@ import React, { useMemo } from "react";
 import AddInventory from "./AddInventory";
 import AddMake from "./AddMake";
 import AddModel from "./AddModel";
+import VehicleDatabase from "./VehicleDatabase";
 
 const AdminToolkit = () => {
   const location = useLocation();
@@ -17,12 +18,16 @@ const AdminToolkit = () => {
         <Link to={`${basePath}/addinventory`}>Add Inventory</Link>
         <Link to={`${basePath}/addmake`}>Add Make</Link>
         <Link to={`${basePath}/addmodel`}>Add Model</Link>
+        <div>
+          <Link to={`${basePath}/vehicledatabase`}>View Vehicle Database</Link>
+        </div>
       </div>
 
       <Routes>
         <Route path="addinventory" element={<AddInventory />} />
         <Route path="addmake" element={<AddMake />} />
         <Route path="addmodel" element={<AddModel />} />
+        <Route path="vehicledatabase" element={<VehicleDatabase />} />
       </Routes>
     </>
   );
