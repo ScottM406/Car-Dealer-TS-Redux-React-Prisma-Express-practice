f0ce3c439f9f93c8a4f2870510cca20f3479005c
Scott <Scottyjmt13@gmail.com>
2025-01-31 21:50:37 -0700

    Add singleVehicle state and reducers. Add funcitonality that renders vehicle info on single vehcile page for vehicle selected.

--- cardealer-frontend/src/components/Inventory/SingleVehiclePage.tsx
+++ cardealer-frontend/src/components/Inventory/SingleVehiclePage.tsx
@@ -1,12 +1,27 @@
 import { useParams } from "react-router-dom";
+import { RootState, AppDispatch } from "../../state/store";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getSpecificVehicle } from "../../state/singleVehicleSlice";
 
 const SingleVehiclePage = () => {
-
   const { id } = useParams();
-
+  const vehicle = useSelector((state: RootState) => state.singleVehicle)
+  const dispatch = useDispatch<AppDispatch>();
+  
+  useEffect(() => {
+    dispatch(getSpecificVehicle(id));
+  }, [])
+  
+  useEffect(() => {
+    console.log(vehicle)
+  }, [])
+  
   return (
     <div id="single-vehicle-block">
-    <h1>{id}</h1>
+    <h1>{vehicle.headline}</h1>
+    <img src={vehicle.image} alt="a photo of the vehcile"/>
+    <h2>{vehicle.description}</h2>
     </div>
   )
 }
