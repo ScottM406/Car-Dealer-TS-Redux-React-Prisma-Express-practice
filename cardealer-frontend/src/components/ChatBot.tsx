d6854b8ecada48577486ced6eb6a5462de0c38e9
Scott <Scottyjmt13@gmail.com>
2025-02-07 18:17:40 -0700

    pass inventory state to chatbot. add reject empty message function.

--- cardealer-frontend/src/components/ChatBot.tsx
+++ cardealer-frontend/src/components/ChatBot.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../state/store";
+import { getVehicles } from "../state/vehiclesSlice";
 
 const key: string = import.meta.env.VITE_GPT_KEY;
 const messageLog: string[] = [];
@@ -8,6 +11,12 @@ const ChatBot: React.FunctionComponent = () => {
   const [message, setMessage] = useState<string>("");
   const [isOpen, setIsOpen] = useState<boolean>(false);
   // const [messageLog, setMessageLog] = useState<string[]>([]) *Perhaps switch to useState when scaling up.
+  const dispatch = useDispatch<AppDispatch>()
+  const vehciles = useSelector((state: RootState) => state.vehicles)
+  
+  useEffect(() => {
+    dispatch(getVehicles)
+  }, [vehciles])
 
   const sendMessage = async (e: any) => {
     e.preventDefault();
@@ -16,17 +25,26 @@ const ChatBot: React.FunctionComponent = () => {
     setMessage("");
   };
 
+  const rejectEmptyMessage = (e:any) => {
+    e.preventDefault();
+  }
+
   const getGPTReply = async () => {
     const GPT = new GoogleGenerativeAI(key);
     const model = GPT.getGenerativeModel({ model: "gemini-1.5-flash" });
-    
-    const prompt = "Hello. I am testing a car dealership web project. At the end of this message will be a user's prompt for the car dealership's 'chatbot'. Please answer the question with a friendly sentance or two in the tone of a chatbot answering a question about a vehcile, service, or the website in genreal. If the user's prompt is not relevant to the workings of a car dealership or it's website. please respond with a sentence explaining that you only answer questions relating to the dealership. I have noticed that oftentimes, your response also includes \n\{original prompt}\n\; Please do not return that. The sentance after this one is the user's prompt." + message;
+    const vehiclesStringified = JSON.stringify(vehciles)
+    const prompt = `Hello. I am testing a car dealership web project. At the end of this message will be a user's prompt for the car dealership's 'chatbot'. 
+    Please answer the question with a friendly sentance or two in the tone of a chatbot answering a question about a vehcile, service, or the website in genreal. 
+    He is an object which contains information on the vehicles currently in stock - ${vehiclesStringified}. 
+    Please use the information contained in the object to anwser vehicle questions. 
+    If the user's prompt is not relevant to the workings of a car dealership or it's website. please respond with a sentence explaining that you only 
+    answer questions relating to the dealership. If the prompt is empty or appears to be accidentally submitted early, please respond that you are a
+    chatbot and tell the user that you are ready to help with their questions. I have noticed that oftentimes, your response also includes \n\{original prompt}\n\; 
+    Please do not return that. The sentance after this one is the user's prompt.` + message;
     const result = await model.generateContent(prompt);
     const resultText = await result.response.text();
     messageLog.push("Chatbot: " + resultText);
   };
-
-  console.log(messageLog);
 
   return (
     <>
@@ -39,7 +57,7 @@ const ChatBot: React.FunctionComponent = () => {
             ))}
           </div>
           <div id="chatbot-prompt">
-            <form onSubmit={sendMessage}>
+            <form onSubmit={ message === "" ? rejectEmptyMessage : sendMessage}>
               <textarea
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
