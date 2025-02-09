5a0c061587f8d00dd2fb96cb8bfb96183e2950b7
Scott <Scottyjmt13@gmail.com>
2025-02-09 13:49:33 -0700

    complete request showing form

--- cardealer-frontend/src/components/Inventory/RequestShowingWidget.tsx
+++ cardealer-frontend/src/components/Inventory/RequestShowingWidget.tsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-const RequestShowingWidget: React.FC = () => {
+interface Props {
+  vehicleID: number
+  vehicleYear: number
+  vehicleMake: string
+  vehicleModel: string
+}
+
+const RequestShowingWidget: React.FC<Props> = ({ vehicleID, vehicleYear, vehicleMake, vehicleModel} ) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
   useEffect(() => {
@@ -15,17 +22,51 @@ const RequestShowingWidget: React.FC = () => {
   return (
     <div id="request-showing-widget">
       <div id="request-showing-widget-block" className={`collapse card card-body ${isOpen ? "show" : ""}`}>
+        <h1>{vehicleYear} {vehicleMake} {vehicleModel}</h1>
         <form>
-          <label>First Name:</label>
-          <input type="text"></input>
-          <label>Last Name:</label>
-          <input type="text"></input>
-          <label>Phone Number:</label>
-          <input type="tel"></input>
-          <label>Email Address:</label>
-          <input type="email"></input>
-          <label>Desired Date and Time:</label>
-          <input type="datetime-local" id="request-showing-widget-datetime-input"></input>
+          <label htmlFor="request-showing-firstname-input">First Name:</label>
+          <input
+          type="text"
+          name="firstname"
+          id="request-showing-firstname-input"
+          required
+          />
+          <label htmlFor="request-showing-lastname-input">Last Name:</label>
+          <input
+          type="text"
+          name="lastname"
+          id="request-showing-lastname-input"
+          required
+          />
+          <label htmlFor="request-showing-telephone-number-input">Phone Number:</label>
+          <input
+          type="tel"
+          name="telephone"
+          id="request-showing-telephone-number-input"
+          required
+          />
+          <label htmlFor="request-showing-email-input">Email Address:</label>
+          <input
+          type="email"
+          name="email"
+          id="request-showing-email-input"
+          required
+          />
+          <label htmlFor="request-showing-widget-datetime-input">Desired Date and Time:</label>
+          <input
+          type="datetime-local"
+          name="datetime"
+          id="request-showing-widget-datetime-input"
+          />
+          <div>
+          <label>Request Showing </label>
+          <input type= "radio" name="showing-type"/>
+          </div>
+          <div>
+          <label>Request Showing and Test Drive: </label>
+          <input type="radio" name="showing-type"/>
+          </div>
+          <button>Request Showing</button>
         </form>
       </div>
     <button
