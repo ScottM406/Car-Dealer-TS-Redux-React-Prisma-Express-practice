b822c355f3683fe3f3e4223d8469569449de5b80
Scott <Scottyjmt13@gmail.com>
2025-02-11 15:06:26 -0700

    change id type in async thunk to string | undefined to fix type error.

--- cardealer-frontend/src/state/singleVehicleSlice.ts
+++ cardealer-frontend/src/state/singleVehicleSlice.ts
@@ -49,7 +49,7 @@ const singleVehicleSlice = createSlice({
 
 export const getSpecificVehicle = createAsyncThunk(
   "vehicles/getSpecificVehicle",
-  async (id) => {
+  async (id: string | undefined) => {
     const response = await fetch(`http://localhost:3000/cars-on-lot/${id}`)
     const vehicle = await response.json();
     return vehicle
